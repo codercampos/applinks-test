946044efc8b9d19c7f023cd15bbccea2ec9401a2
Carlos Campos <me@carloscampos.dev>
2022-06-28 00:24:20 -0600

    App Links sample

---
+++ App1/App1.Droid/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Content;
+using Android.OS;
+using Xamarin.Forms.Platform.Android.AppLinks;
+
+namespace App1.Droid
+{
+    [Activity(Label = "App1", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] {Intent.ActionView}, Categories = new [] {Intent.CategoryDefault, Intent.CategoryBrowsable},
+        DataScheme = "cirroByTsd",
+        AutoVerify = true)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            if (this.Intent.Data != null)
+            {
+                var data = this.Intent.Data.ToString();
+                data = data.Replace("cirroByTsd://", "https://tsdweb.com/?q=");
+                this.Intent.SetData(Android.Net.Uri.Parse(data));
+            }
+            LoadApplication(new App());
+
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+        }
+    }
+}
---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnAppLinkRequestReceived(Uri uri)
+        {
+            base.OnAppLinkRequestReceived(uri);
+            var text = uri.ToString();
+            text = text.Replace("https://tsdweb.com/?q=", "");
+            var splitted = text.Split(new []{"&"}, StringSplitOptions.RemoveEmptyEntries);
+            splitted.ForEach(Console.WriteLine);
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
